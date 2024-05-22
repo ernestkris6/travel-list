d9a733aaeb792474a696845453a0422763760571
ERNEST CHRIS <ernestchris6@gmail.com>
2024-05-22 23:52:29 +0100

    mapping thru

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './index.css';
 //Map
 //Filter
 //Sorting
+const initialItems = [
+    {
+        "id" : 1,
+        "description" : "Socks",
+        "packed" : false,
+        "quantity" : 3
+    },
+    {
+        "id" : 2,
+        "description" : "Charger",
+        "packed" : false,
+        "quantity" : 3
+    },
+    {
+        "id" : 3,
+        "description" : "Passport",
+        "packed" : true,
+        "quantity" : 3
+
+    },
+]
 
 const App = () =>{
 
@@ -20,16 +41,21 @@ const App = () =>{
 
 
 function Logo(){
-    return <h1>🏝 FAR AWAY 💼</h1>
+    return <h1> 🏝 FAR AWAY 💼 </h1>
 }
 function Form(){
+    function handleSubmit(e){
+        e.preventDefault()
+    }
     return(
         <div className="add-form">
             
-            <form>
+            <form onSubmit={handleSubmit}>
             <span>What do you need for your 😍 trip?</span>
                 <select>
-                    <option>1</option>
+                   {Array.from({length:20}, (_, i) => i + 1).map((num) =>
+                    <option value={num} key={num}>{num}</option>
+                )}
                 </select>
                 <input type="text" placeholder="item..." />
                 <button>ADD</button>
@@ -40,16 +66,35 @@ function Form(){
 
 function PackingList(){
     return (
-        <div className="list">
-            <p>Socks</p>
-        </div>
+        <ul className="list">
+            {initialItems.map((item)=> (
+                <Item item={item} key={item.id}/>
+            ))}
+        </ul>
+    )
+}
+
+function Item({item}){
+    return (
+        <li>
+            {item.packed ? (<span  
+            style={{textDecoration : "line-through"}}>
+                {item.description}
+                {item.quantity}
+                </span>) : null 
+                }
+            <button>&times;</button>
+        </li>
     )
 }
 
 function Stats(){
     return(
         <footer className="stats">
-            
+           <em>
+           <p>You have X items on your list, 
+            and you already packed X (X%)</p>
+           </em>
         </footer>
     )
 }
